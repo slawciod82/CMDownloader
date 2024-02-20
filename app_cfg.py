ba79fea029df48c80aeef5ddb6140b2e2b4d4978
slawciod82@gmail.com <slawciod82@gmail.com>
2024-02-20 14:50:21 +0100

    cm menago 001

---
+++ app_cfg.py
@@ -0,0 +1,10 @@
+api_psychologia = "euf90cd52e900a4245196929cc6037b2a34aea3d64"
+api_ah200 = "eu0970f0e5d774d09189ec5030af8dfd82e6e942dd"
+api_ah500 = "eu21245290f97d71d0bb9323d97f0eb79b50b9f4fc"
+api_ah1550 = "eu2dbda1d93d854803d4d4872415b342a89ecd8919"
+api_ah5100 = "euea89422400467515f5fc5815701100d1a39e58e4"
+
+apikeys = [api_psychologia, api_ah200, api_ah500, api_ah1550, api_ah5100]
+account_names = ['api_psychologia', 'api_ah200', 'api_ah500', 'api_ah1550', 'api_ah5100']
+
+path_to_save = str('/home/osboxes/test')
