7a911246b665f1779978c5336f7728454f24d33b
slawciod82 <107408241+slawciod82@users.noreply.github.com>
2024-02-21 08:42:59 +0100

    Create app_cfg_example.py

---
+++ app_cfg_example.py
@@ -0,0 +1,8 @@
+api_1 = "api_kay_1"
+api_2 = "api_kay_2"
+api_3 = "api_kay_3"
+
+apikeys = [api_1, api_2, api_3]
+account_names = ['api_name_1', 'api_name_2', 'api_name_3']
+
+path_to_save = f'/yours/path/goes/here'
